eaf2ff425ca2acd4161dc682df8cbfb4b8cacbe0
Mark Nyder <mneider.virginia@gmail.com>
2019-01-01 17:46:27 -0600

    display admin menu

--- LifeLog.Console/Program.cs
+++ LifeLog.Console/Program.cs
@@ -10,8 +10,42 @@ namespace LifeLog.Console
     {
         static void Main(string[] args)
         {
-            System.Console.Title = APP_TITLE;
+            SetupConsole();
+            
+            MenuOption selection = GetSelection();
+
             System.Console.ReadLine();
+        }
+
+        private static void SetupConsole()
+        {
+            System.Console.Title = APP_TITLE;
+        }
+
+        private static void DisplayMenu()
+        {
+            System.Console.WriteLine("{0}) Install LifeLog", (int) MenuOption.InstallLifeLog);
+            System.Console.WriteLine("{0}) Quit", (int) MenuOption.Quit);
+        }
+
+        private static MenuOption GetSelection()
+        {
+            string input;
+            MenuOption selection;
+            do
+            {
+                DisplayMenu();
+                input = System.Console.ReadLine();
+
+            } while (!(Enum.TryParse(input, out selection) && Enum.IsDefined(typeof(MenuOption), selection)));
+
+            return selection;
+        }
+        
+        private enum MenuOption
+        {
+            InstallLifeLog = 1,
+            Quit,
         }
 
         private const string APP_TITLE = "LifeLog Admin";
